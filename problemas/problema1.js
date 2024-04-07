3a2d51716994b53b58243bda6c5d6c3a9bdf77e0
Franco Calegari <francocalegari55@gmail.com>
2024-04-07 20:59:34 -0300

    ictoria

--- problemas/problema1.js
+++ problemas/problema1.js
@@ -1,11 +1,12 @@
 //Problema A
 document.addEventListener('DOMContentLoaded', function() {
-    // Obtener una referencia al botón
+    // Obtener una referencia al botón, importar cosas que primero necesitas del html
     let boton = document.getElementById('btnP1');
+    let cofla = document.querySelector('#CoflaIN');
 
     // Agregar un evento de clic al botón
     boton.addEventListener('click', function() {
-        let cofla = prompt("Cuanto dinero tiene cofla: ");
+        let coflaValor =  cofla.value;//Luego se guarda el valor que antes se importo en otra variable
         // let roberto = prompt("Cuanto dinero tiene cofla: ");
         // let pedro = prompt("Cuanto dinero tiene cofla: ");
 
@@ -13,26 +14,26 @@ document.addEventListener('DOMContentLoaded', function() {
         let roberto = 1.5;
         let pedro = 1.7;
 
-        if (cofla >= 0.6 && cofla < 1) {
+        if (coflaValor >= 0.6 && coflaValor < 1) {
             alert("Comprate el helado de agua")
         }
-        else if (cofla >= 1 && cofla < 1.6 ) {
+        else if (coflaValor >= 1 && coflaValor < 1.6 ) {
             alert("Comprate el helado de crema")
         }
 
-        else if (cofla >= 1.6 && cofla < 1.7 ) {
+        else if (coflaValor >= 1.6 && coflaValor < 1.7 ) {
             alert("Comprate el helado de helardix")
         }
 
-        else if (cofla >= 1.7 && cofla < 1.8 ) {
+        else if (coflaValor >= 1.7 && coflaValor < 1.8 ) {
             alert("Comprate el helado de heladovich")
         }
 
-        else if (cofla >= 1.8 && cofla < 2.9 ) {
+        else if (coflaValor >= 1.8 && coflaValor < 2.9 ) {
             alert("Comprate el helado de helardo")
         }
 
-        else if (cofla >= 2.9) {
+        else if (coflaValor >= 2.9) {
             alert("Comprate el pote de 1/4KG")
         }
 
